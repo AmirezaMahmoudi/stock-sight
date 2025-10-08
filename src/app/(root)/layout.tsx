062a9b287b760d6f17677ba02b362eae37ab3adf
AmirrezaMahmoudi <amirrezamh03@gmail.com>
2025-10-08 14:59:52 +0330

    implment header

---
+++ src/app/(root)/layout.tsx
@@ -0,0 +1,18 @@
+import Header from "@/components/layout/header";
+import React from "react";
+import "../globals.css";
+
+const Layout = ({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) => {
+  return (
+    <main className="min-h-screen text-gray-400">
+      <Header />
+      <div className="container py-10">{children}</div>
+    </main>
+  );
+};
+
+export default Layout;
